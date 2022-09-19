90d32353e3e3572c3dbd44c071da53f37364cab9
sanyi <89388003@qq.com>
2022-09-20 01:56:35 +0800

    99

--- apps/train.js
+++ apps/train.js
@@ -75,6 +75,22 @@ export class train extends plugin {
                 this.reply(`训练的还行\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability}`)
             }
         }
+        else if (riqi != nowday() && cishu > 2) {
+            favorability += value
+            cishu = 0
+            await redis.set(`Yz:sanyi0:favorability:${a}:${b}:favorability`, favorability)
+            await redis.set(`Yz:sanyi0:favorability:${a}:${b}:cishu`, cishu)
+            await redis.set(`Yz:sanyi0:favorability:${a}:${b}:riqi`, nowday())
+            if (value > 0) {
+                this.reply(`训练很卖力\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability}`)
+            }
+            else if (value < 0) {
+                this.reply(`训练心不在焉的\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability}`)
+            }
+            else {
+                this.reply(`训练的还行\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability}`)
+            }
+        }
         else {
             var haogan= await redis.get(`Yz:sanyi0:favorability:${a}:${b}:favorability`)
             this.reply(`@${b}\n今天已经训练好久了，休息一下明天再来吧!当前好感度`+haogan)
