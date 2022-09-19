806398f3a6a928f312fd848ec32c073f7b47cf6c
sanyi <89388003@qq.com>
2022-09-19 20:51:04 +0800

    te

--- apps/train.js
+++ apps/train.js
@@ -61,7 +61,7 @@ export class train extends plugin {
             this.e.reply("fnc1")
 
         }
-        else if (trainriqi != Number(riqi())) {
+        else if (Number(trainriqi) != Number(riqi())) {
 
             this.training(favorability, cishu, value, a, b)
             await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, 1)
