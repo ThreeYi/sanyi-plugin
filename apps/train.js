018257fee3f7c5eaf37fc4a470aa44296859fcc9
sanyi <89388003@qq.com>
2022-09-19 14:33:33 +0800

    修改好感度玩法

--- apps/train.js
+++ apps/train.js
@@ -22,44 +22,43 @@ export class train extends plugin {
         return da
 
     }
+    async training() {
+        favorability += value
+        cishu += 1
+        await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, favorability)
+        await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, cishu)
+        if (value > 0) {
+            await this.reply(`训练很卖力呢，恭喜！\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability}`)
+        }
+        else if (value < 0) {
+            await this.reply(`训练心不在焉的，很遗憾！\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability}`)
+        }
+        else {
+            await this.reply(`训练挺好的！\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability}`)
+        }
+    }
     async changeFavorability(a, b, value) {
         // 先获取 甲 对 乙 的好感度
         let favorability = await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`)
-        if (!favorability || (typeof favorability == "number") || (typeof favorability == "string")) {
-            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, [this.riqi(), 0, 0]) //0:riqi 1:cishu 2：haogandu
+        let trianriqi = await redis.get(`Yz:sanyi:favorability:${a}:${b}:trianriqi`)
+        let cishu = await redis.get(`Yz:sanyi:favorability:${a}:${b}:cishu`)
+        if (!favorability || !riqi || !cishu) {
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, 0)
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:trainriqi`, this.riqi())
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, 0)
         }
-        favorability[2] = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`)[2])
-       
-        
-        if ((favorability[0] = this.riqi()) & favorability[1] < 5) {
-            favorability[2] += value
-            favorability[1] +=1
-            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, favorability)
-            if (value > 0) {
-                await this.reply(`训练很卖力呢，恭喜！\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability[2]}`)
-            }
-            else if (value < 0) {
-                await this.reply(`训练心不在焉的，很遗憾！\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability[2]}`)
-            }
-            else {
-                await this.reply(`训练挺好的！\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability[2]}`)
-            }
+        favorability = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`))
+        trianriqi = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:trainriqi`))
+        cishu = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:cishu`))
+
+        if ((trainriqi = this.riqi()) & cishu < 5) {
+
+            this.training()
 
         }
-        else if (favorability[0] != this.riqi()) {
-            favorability[2] += value
-            favorability[1] +=1
-            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, favorability)
+        else if (trainriqi != this.riqi()) {
 
-            if (value > 0) {
-                await this.reply(`训练很卖力呢，恭喜！\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability[2]}`)
-            }
-            else if (value < 0) {
-                await this.reply(`训练心不在焉的，很遗憾！\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability[2]}`)
-            }
-            else {
-                await this.reply(`训练挺好的！\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability[2]}`)
-            }
+            this.training()
 
         }
         else {
