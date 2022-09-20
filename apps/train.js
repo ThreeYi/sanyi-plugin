7ef827816cdf15ea331063013df8b6b04f8d37ef
sanyi <89388003@qq.com>
2022-09-20 12:01:59 +0800

    训练测试

--- apps/train.js
+++ apps/train.js
@@ -34,8 +34,63 @@ export class train extends plugin {
     async xunlian(e) {
         
     }
-    async xun(e){
+    async changeFavorability(a, b, value) {
+        // 先获取 甲 对 乙 的好感度
+        let favorability = await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`)
+        let riqi = (await redis.get(`Yz:sanyi:favorability:${a}:${b}:riqi`))
+        let cishu = (await redis.get(`Yz:sanyi:favorability:${a}:${b}:cishu`))
+        if (!favorability) {
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, '0')
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:riqi`, '0')
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, '0')
+        }
+        favorability = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`))
+        riqi = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:riqi`))
+        cishu = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:cishu`))
+        if ( String(riqi)==nowday() && cishu < 3 ){
+            // favorability = Number(await redis.get(`Yz:sanyi:favorability:${a}:${b}:favorability`))
+            favorability += value
+            cishu += 1
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, String(favorability))
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, String(cishu))
+            if (value > 0) {
+                this.reply(`训练很卖力\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability}`)
+            }
+            else if (value < 0) {
+                this.reply(`训练心不在焉的\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability}`)
+            }
+            else {
+                this.reply(`训练表现平平\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability}`)
+            }
+        }
+        else if( String(riqi) != nowday()) {
+            cishu =0
+            favorability += value
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:favorability`, String(favorability))
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:cishu`, String(cishu))
+            await redis.set(`Yz:sanyi:favorability:${a}:${b}:riqi`, nowday())
+            if (value > 0) {
+                this.reply(`训练很卖力\n${a} 对 ${b} 的好感度增加了 ${value}\n当前好感度: ${favorability}`)
+            }
+            else if (value < 0) {
+                this.reply(`训练心不在焉的\n${a} 对 ${b} 的好感度降低了 ${Math.abs(value)}\n当前好感度: ${favorability}`)
+            }
+            else {
+                this.reply(`训练表现平平\n${a} 对 ${b} 的好感度不变呢\n当前好感度: ${favorability}`)
+            }
+        }
 
+        else {
+            e.reply(`@${b}\n今天已经训练很久了，休息一下明天再来吧`)
+        }
+       
+        
+    }
+    async xun(e){
+        b=e.nickname
+        value = -1
+        value += Math.ceil(Math.random() * 5)
+        this.changeFavorability(a,b)
         e.reply(nowday())
       
 }
