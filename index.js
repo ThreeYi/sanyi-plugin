c88f5d2014aecaa3b7bef1112ded0c703143db0a
sanyi <89388003@qq.com>
2022-10-30 20:26:58 +0800

    修改index

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-import fs from "node:fs";
+import fs from 'node:fs'
+import Ver from './components/Version.js'
+
+const files = fs.readdirSync('./plugins/sanyi-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info('-------------------------')
+logger.info(`三一插件${Ver.ver}初始化~`)
+logger.info('-------------------------')
 
 
-logger.info(`-----------加载三一插件---------------`);
+files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
 
-const files = fs
-  .readdirSync("./plugins/sanyi-plugin/apps")
-  .filter((file) => file.endsWith(".js"));
+ret = await Promise.allSettled(ret)
 
-let apps = {};
-for (let file of files) {
-  let name = file.replace(".js", "");
-  apps[name] = (await import(`./apps/${file}`))[name];
+let apps = {}
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
-
-export { apps };
+export { apps }
