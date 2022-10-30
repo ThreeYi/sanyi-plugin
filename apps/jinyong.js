aa9e3cb7ba44aade99852338b0b02f0f555dc8be
sanyi <89388003@qq.com>
2022-10-30 20:48:45 +0800

    1、修改更新，2、修改上下班提示

--- apps/jinyong.js
+++ apps/jinyong.js
@@ -10,7 +10,7 @@ export class jinyong extends plugin {
     constructor() {
         super({
             name: "机器人群开关",
-            dsc: "控制机器人在指定群开关，禁绝所有",
+            dsc: "控制机器人在指定群开关",
             event: "message",
             priority: 2,
             rule: [{
@@ -36,7 +36,7 @@ export class jinyong extends plugin {
             data[e.group_id] = { enable: ["机器人群开关", ] }
             let yaml = YAML.stringify(data)
             fs.writeFileSync(this.file, yaml, "utf8")
-            e.reply(`${botname}${closetip}`)
+            e.reply(closetip)
         } else {
             e.reply('请在群聊中使用')
         }
@@ -50,7 +50,7 @@ export class jinyong extends plugin {
             data[e.group_id] = { enable: null }
             let yaml = YAML.stringify(data)
             fs.writeFileSync(this.file, yaml, "utf8")
-            e.reply(`${botname}${starttip}`)
+            e.reply(starttip)
         } else {
             e.reply('请在群聊中使用')
         }
