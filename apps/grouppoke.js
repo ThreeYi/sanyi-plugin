29d11a467696b40a04a01acaf9e08828f9dc1f75
sanyi <89388003@qq.com>
2022-11-06 17:18:50 +0800

    修改配置文件

--- apps/grouppoke.js
+++ apps/grouppoke.js
@@ -18,20 +18,23 @@ export class grouppoke extends plugin {
 
 
     async grouppoke(e) {
-        let poke_switch=await sycfg.get_cfg('bot.yaaml',poke_switch)
+        let poke_switch=await sycfg.get_cfg('bot.yaml','poke_switch')
         if(poke_switch == false){
             return false
         }
-        let words=await sycfg.get_cfg('grouppoke_words.yaml','poked_words')
-        let wordsindex = Math.ceil(Math.random() * words.length)
+        
         let appindex = Math.ceil(Math.random() * 2)
         if (e.target_id == cfg.qq) {
             switch (appindex) {
                 case 1:
-                    e.reply(words[wordsindex - 1])
+                    let poked_words=await sycfg.get_cfg('grouppoke_words.yaml','poked_words')
+                    let wordsindex = Math.ceil(Math.random() * poked_words.length)
+                    e.reply(poked_words[wordsindex - 1])
                     break
                 case 2:
-                    this.reply('你刚刚是不是戳我了，我要戳回去，哼！！！')
+                    let poke_words=await sycfg.get_cfg('grouppoke_words.yaml','poke_words')
+                    let wordsindex2 = Math.ceil(Math.random() * poke_words.length)
+                    e.reply(poke_words[wordsindex2 - 1])
                     await common.sleep(1000)
                     e.group.pokeMember(e.operator_id)
                     break
